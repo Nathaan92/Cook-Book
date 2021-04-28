a2ba58c3bbff388abbe8fdf2ab2a8832a2bd9b00
Nathaniel Reister <nathaniel.reister@gmail.com>
2021-04-27 21:06:54 -0700

    Creation of App, add entity framework, creation of model

---
+++ Code/Cook Book/Cookaboo/Cookaboo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Cookaboo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
